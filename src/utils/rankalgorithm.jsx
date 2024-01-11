7a5fdacd8b2c69f363533d806672c2004d3942b7
Laman Hasanli <hesenlileman2001@gmail.com>
2024-01-11 02:50:09 +0100

    Implement API for users' website

---
+++ src/utils/rankalgorithm.jsx
@@ -0,0 +1,82 @@
+// temp, hum, noise
+
+export const getScores = (RoomCriteriaMat, criteriaMat) => {
+  //variables needed
+  let n = RoomCriteriaMat[0].length;
+  const tempMax = 25;
+  const tempMin = 21;
+  const humMax = 70;
+  const humMin = 40;
+  const noiseMax = 60;
+  const noiseMin = 0;
+
+  //Normalization of the values in a matrix
+  let RoomCriteriaNormalized = [];
+  //normalize temperature
+  RoomCriteriaNormalized[0] = [];
+  for (let i = 0; i < n; i++) {
+    const x = (RoomCriteriaMat[0][i] - tempMin) / (tempMax - tempMin);
+    if (x < 0 || x > 1) {
+      RoomCriteriaNormalized[0][i] = 0;
+    } else {
+      RoomCriteriaNormalized[0][i] = x;
+    }
+  }
+
+  //normalize humidity
+  RoomCriteriaNormalized[1] = [];
+  for (let i = 0; i < n; i++) {
+    const x = (RoomCriteriaMat[1][i] - humMin) / (humMax - humMin);
+    if (x < 0 || x > 1) {
+      RoomCriteriaNormalized[1][i] = 0;
+    } else {
+      RoomCriteriaNormalized[1][i] = x;
+    }
+  }
+
+  //normalize noise
+  RoomCriteriaNormalized[2] = [];
+  for (let i = 0; i < n; i++) {
+    const x = (RoomCriteriaMat[2][i] - noiseMin) / (noiseMax - noiseMin);
+    if (x < 0 || x > 1) {
+      RoomCriteriaNormalized[2][i] = 0;
+    } else {
+      RoomCriteriaNormalized[2][i] = x;
+    }
+  }
+
+  //create weight for each criteria accoring to its importance
+  var sum = criteriaMat.reduce((a, b) => a + b, 0);
+  const criteriaNormalized = [];
+  for (let i = 0; i < criteriaMat.length; i++) {
+    criteriaNormalized[i] = criteriaMat[i] / sum;
+  }
+
+  //give a score to each room
+  let scores = [];
+  for (let j = 0; j < n; j++) {
+    let RoomScore = 0;
+    for (let i = 0; i < criteriaMat.length; i++) {
+      //for each room we calculate the score
+      RoomScore += RoomCriteriaNormalized[i][j] * criteriaNormalized[i];
+    }
+    scores[j] = RoomScore;
+  }
+
+  return scores;
+};
+
+export const getDataWithScores = (data, criteria) => {
+  const tempArr = data.map((room) => room.temperature);
+  const soundArr = data.map((room) => room.temperature);
+  const humidityArr = data.map((room) => room.temperature);
+  //   const lightArr = data.map((room) => room.temperature);
+
+  const sensorsMat = [tempArr, humidityArr, soundArr];
+  const scores = getScores(sensorsMat, criteria);
+  const dataWithScores = data.map((room, index) => {
+    return { ...room, score: scores[index] };
+  });
+  const sortedData = dataWithScores.sort((a, b) => b.score - a.score);
+  return sortedData;
+};
--- src/utils/timeUtils.jsx
+++ src/utils/timeUtils.jsx
@@ -1,18 +1,15 @@
 export const getLast24Hours = () => {
   const hours = [...Array(24).keys()];
-  const currentHour =
-    new Date().getMinutes() < 30
-      ? new Date().getHours() - 1
-      : new Date().getHours();
+  const currentHour = new Date().getHours();
 
   const last24Hours = [
     ...hours.slice(hours.indexOf(currentHour) + 1),
     ...hours.slice(0, hours.indexOf(currentHour) + 1),
   ];
 
-  const last24HoursFormatted = last24Hours
-    .map((el) => [`${el}:00`, `${el}:30`])
-    .flat();
+  const last24HoursFormatted = last24Hours.map(
+    (el) => `${`${el}`.padStart(2, "0")}:00`
+  );
 
   return last24HoursFormatted;
 };
